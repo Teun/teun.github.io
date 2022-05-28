3f1b3895c28b7f41b9cfff5cbec08f4eaab7ea72
Teun Duynstee <teun@duynstee.com>
2022-05-28 23:32:57 +0200

    test4

--- speurpleur/test4/scripts.js
+++ speurpleur/test4/scripts.js
@@ -1,11 +1,11 @@
 function geoFindMe(status, resultFound, resultNotFound, lat, lng, letter, nextLetter) {
     const target = {latitude: lat, longitude: lng};
+    var watch = 0;
 
     function success(position) {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
         const curr = {latitude, longitude};
-        var watch = 0;
         
         const dist = window.geolib.getDistance(curr, target, accuracy = 1);
 
@@ -14,9 +14,10 @@ function geoFindMe(status, resultFound, resultNotFound, lat, lng, letter, nextLe
             return;
         }
         const dir = window.geolib.getCompassDirection(curr, target);
-
-        status.innerHTML = `<p>Punt <span class="blue">${letter}</span></p><p>is op ${dist} meter</p><p>in richting <span class="compass">${dir}</span>.</p>
-        <p>[accuracy: ${position.coords.accuracy}, speed: ${position.coords.speed}, direction: ${position.coords.heading}]</p>`;
+        const icons = getIconsFor(position.coords, curr, target);
+        const track = `<p>Punt <span class="blue">${letter}</span></p><p>is op ${dist} meter</p><p>in richting <span class="compass">${dir}</span>.</p>`;
+        const extra = `        <p>[accuracy: ${position.coords.accuracy}, speed: ${position.coords.speed}, direction: ${position.coords.heading}]</p>`;
+        status.innerHTML = icons + track + extra;
     }
 
     function error() {
@@ -37,4 +38,12 @@ function geoFindMe(status, resultFound, resultNotFound, lat, lng, letter, nextLe
         watch = navigator.geolocation.watchPosition(success, error);
     }
 
+}
+function getIconsFor(coords, curr, target) {
+    if (coords.accuracy > 20) return "Status: Zwak GPS signaal";
+    var walker = String.fromCodePoint(0x1f9cd);
+    if (coords.speed > .5) walker = String.fromCodePoint(0x1f6b6);
+    if (coords.speed > 1.2) walker = String.fromCodePoint(0x1f3c3);
+
+    return `Status: ${walker}`;
 }
